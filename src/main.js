11757834676ab30432e1c6500bcfb85259f9afe3
AnnaMatsieva <ann.matsieva@gmail.com>
2017-07-13 15:49:17 +0300

    some info

---
+++ src/main.js
@@ -0,0 +1,55 @@
+/*111111111111111111111111111111111111111111111111111111*/
+/*input text what print in input*/
+/*angular.module('app', []).controller('myCtrl', function($scope){
+    $scope.hello = 55;
+    $scope.someText = "gwerty";
+    $scope.someInputText = "asdf";
+});*/
+
+
+/*222222222222222222222222222222222222222222222222222*/
+//Factory обмен данными между несколькими контроллерами
+var app = angular.module('app', []);
+
+/*app.controller('firstCtrl', function($scope, myFactory){
+    $scope.myFactory = myFactory; //заносим значение в scope
+    $scope.hello = 'hello';
+});
+
+app.controller('secondCtrl', function($scope, myFactory){
+    $scope.myFactory = myFactory; //заносим значение в scope
+    $scope.hello = 'hello';
+});
+
+app.factory('myFactory', function(){
+    return{
+        hello: 'hello world'
+    };
+});*/
+
+
+/*33333333333333333333333333333333333333333333333333333333*/
+/*Filter изменение вывода денег*/
+
+app.controller('moneyCtrl', function(){
+    this.money1 = '1.22$';
+    this.money2 = '$1.33';
+    this.money3 = '1.45';
+});
+
+app.filter('moneyFilter', function(){
+    return function(str){
+        var lastChar = str.slice(-1),
+            firstChar = str.slice(0, 1),
+            slicePart;
+        
+        if (lastChar === '$'){
+            slicedPart = str.slice(0, str.length - 1);
+            return '$' + slicedPart;
+        } else if (firstChar === '$'){
+            return str;
+        } else {
+            return '$' + str;
+        }
+    }    
+});
